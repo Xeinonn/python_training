2b84340379f4c827a352f12c849aa0cb8af54dab
Xeinonn <xeinonn@googlemail.com>
2022-04-11 16:56:22 +0200

    Added ariable exercise and renamed files

---
+++ variable_training_3.py
@@ -0,0 +1,10 @@
+#Short text to train variables
+special_candy = "karameliki"
+my_age = "8"
+candy_form = "chicken"
+print("When I was small, I liked a special candy, which was made of melted sugar. We called it as kids " + special_candy + ".")
+print("Back then I was around " + my_age + " years old.")
+print("There were different forms of " + special_candy + ".")
+print("I liked the form of a " + candy_form + ". Because this one was the biggest.")
+print("My friend liked also the form of a " + candy_form + " back then.")
+print("As far I can remember, being an " + my_age + " year old kid was fun and I enjoyed it a lot.")
