24b4eee01ba3d5860375587f939a91fd76ea9276
Xeinonn <xeinonn@googlemail.com>
2022-08-24 19:24:41 +0300

    Testing and working with numbers

---
+++ numbers.py
@@ -0,0 +1,22 @@
+#Just print a number 2.
+print(2)
+#Print of a decimal number.
+print(123.98)
+#A print of a negative number.
+print(-75.61)
+#Multiplication example.
+print(6*2)
+#Summation.
+print(7+6)
+#Usage of parentesis with an example.
+print(3*(5+10))
+#Modular example, which means here 9/3 and ramains 1.
+print(10%3)
+Number_1 = 794
+Number_2 = 206
+Number_3 = 8
+#Variable usage example with numbers.
+print (Number_1 + Number_2)
+#Usage of a str function before a number to display it as a string.
+print(str(Number_3) + " is my number.")
+#Expona
