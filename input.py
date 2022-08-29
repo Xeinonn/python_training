89c0c3a418bd1c1a94c5ce4597fd2515df6671bc
Xeinonn <xeinonn@googlemail.com>
2022-08-29 17:04:12 +0300

    Working with user input in a simple way.

---
+++ input.py
@@ -0,0 +1,5 @@
+#basic input
+var_name = input("What is your name? ")
+var_money = input("How much money do you have on you " + var_name + "? ")
+print("You have a beatifull name " + var_name + " =)")
+print("You seem to be a nice guy " + var_name + "." + " Now reach me your " + var_money + " and leave before I break your ankles.")
