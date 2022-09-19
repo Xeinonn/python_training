0b0aff39af630c7f3859cae6faae0306aeba34c5
Xeinonn <xeinonn@googlemail.com>
2022-09-19 15:16:02 +0200

    correct typo

--- input.py
+++ input.py
@@ -1,5 +1,8 @@
-#basic input
+#basic input name.
 var_name = input("What is your name? ")
+#Input about moeny quantity and we use the output of the previous input var_name.
 var_money = input("How much money do you have on you " + var_name + "? ")
-print("You have a beatifull name " + var_name + " =)")
+#Default print with usage of the output from the variable var_name.
+print("You have a beautiful name " + var_name + " =)")
+#print with the combined output of two variables var_name and var_money. Simple string added at the end.
 print("You seem to be a nice guy " + var_name + "." + " Now reach me your " + var_money + " and leave before I break your ankles.")
