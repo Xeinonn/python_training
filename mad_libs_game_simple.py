15388d63b48f15183bca9b65e1269d3dc7e8e6e6
Xeinonn <xeinonn@googlemail.com>
2022-09-19 17:15:35 +0200

    mad libs game created.

---
+++ mad_libs_game_simple.py
@@ -0,0 +1,7 @@
+#Nothing special, maybe I could add later some indicator for wrong and right input.
+input_1 = input("Which flowers are red and symbolize love:")
+input_2 = input("The color of the flowers which symbolize innocence is:")
+input_3 = input("There is one country in Africa... =) :")
+print(input_1 + " are red.") 
+print("Violets are " + input_2 + ".") 
+print("Everybody in " + input_3 + " knows kung fu.")
