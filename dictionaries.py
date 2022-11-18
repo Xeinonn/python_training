f22df926e9b9e7483ebab3e22079e955f350b6f6
Xeinonn <xeinonn@googlemail.com>
2022-11-18 10:53:33 +0100

    First time building a dictionarie

---
+++ dictionaries.py
@@ -0,0 +1,16 @@
+#Is a way to store data. Here the data is stored in so called keys. You can call a key and the assigned data will appear.
+favourite_games = {
+    0: "Prince of Persia",
+    1: "Warcraft frozen throne",
+    2: "Heroes of might and magic",
+    3: "Dota",
+    4: "Battlefield 4",
+    5: "Gunz",
+    6: "Crusaders",
+    7: "Total War Rome",
+    8: "Medieval total war",
+    9: "Mount and blade",
+    10: "CS Go Global offensive",
+}
+print(type(favourite_games))
+print(favourite_games[1])
