28c977e0eaea1467cb97ca00a5c96c8121881f06
Xeinonn <xeinonn@googlemail.com>
2023-01-16 18:08:22 +0100

    Added comment

--- input.py
+++ input.py
@@ -6,3 +6,4 @@ var_money = input("How much money do you have on you " + var_name + "? ")
 print("You have a beautiful name " + var_name + " =)")
 #print with the combined output of two variables var_name and var_money. Simple string added at the end.
 print("You seem to be a nice guy " + var_name + "." + " Now reach me your " + var_money + " and leave before I break your ankles.")
+#Prodoljem kod tyt
